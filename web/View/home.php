9d4db0a25c698a24737f1fcd5ab7193e6fcf2ac9
Marc <marcusterofp23@ibf.cat>
2024-11-08 23:00:11 +0100

    FondoHomeVideo1

--- web/View/home.php
+++ web/View/home.php
@@ -1,5 +1,5 @@
 <div class="homeBC">
-    <video autoplay muted loop id="background-video" src="img/fondov.mp4" type="video/mp4"></video>
+    <video autoplay muted loop id="background-video" src="assets/img/fondov.mp4" type="video/mp4"></video>
     <!--<div class="capa-negra"></div>-->
     <div class="contenido-home">
         <h1>DISFRUTA DE LAS MEJORES</h1>
@@ -7,7 +7,7 @@
         <a href="productos" class="enlace-boton">
             <button>Ver Carta</button>
         </a>
-        <h1>Prueba</h1>
+        
         
         
     </div>
